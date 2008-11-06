3b90f303b16c5cc25541c927a5cd644981d5b7a0
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-11-06 17:40:25 -0400

    removed workshops and workshop registration administration.

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Event, Registration, Workshop, WorkshopRegistration
+from models import Event, Registration
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'starts', 'ends', 'registration_closes',
@@ -13,15 +13,5 @@ class RegistrationAdmin(admin.ModelAdmin):
         'status')
     search_fields = ('first_name', 'last_name', 'notes')
 
-class WorkshopAdmin(admin.ModelAdmin):
-    list_display = ('name', 'title', 'duration', 'cost', 'registrations_count')
-    list_filter = ('event',)
-    prepopulated_fields = {'slug': ('name',)}
-
-class WorkshopRegistrationAdmin(admin.ModelAdmin):
-    list_display = ('registration', 'workshop', 'registered')
-
 admin.site.register(Event, EventAdmin)
 admin.site.register(Registration, RegistrationAdmin)
-admin.site.register(Workshop, WorkshopAdmin)
-admin.site.register(WorkshopRegistration, WorkshopRegistrationAdmin)
